1927a567359399690555fe470ac564587ecb02af
rabeb jerbi <rabebjerbi031@gmail.com>
2026-04-06 15:53:35 +0100

    first commit

---
+++ test_vuln.py
@@ -0,0 +1,8 @@
+import sqlite3
+
+def get_user(user_id):
+    conn = sqlite3.connect("db.sqlite3")
+    cursor = conn.cursor()
+    query = "SELECT * FROM users WHERE id = " + user_id
+    cursor.execute(query)
+    return cursor.fetchone()
